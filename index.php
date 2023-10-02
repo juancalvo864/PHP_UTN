61002b6f09b628e43f2184cdb21050c09d86ca07
Juan <juan.calvo.dev@gmail.com>
2023-10-02 16:18:54 -0300

    correccion ultimo commit

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 
-    <?php include("header.php")?>
-    <h1>ACTIVIDAD NRO 3</h1>
-    <!-- Esto es un comentario -->
-    <p><?php echo "MI" ?></p>
-    <p><?php echo "SEGUNDO" ?></p>
-    <p><?php echo "PHP" ?></p>
-
+<?php include("header.php")?>
+    <section class="body_activity4">
+        <h1>ACTIVIDAD NRO 3</h1>
+        <!-- Esto es un comentario -->
+        <p><?php echo "MI" ?></p>
+        <p><?php echo "SEGUNDO" ?></p>
+        <p><?php echo "PHP" ?></p>
+    </section>
     
 </body>
 </html>
