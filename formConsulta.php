acdf4d615891ea5191299191a8dacdee1e9a6d95
Juan <juan.calvo.dev@gmail.com>
2023-10-02 15:08:36 -0300

    task final

--- formConsulta.php
+++ formConsulta.php
@@ -2,7 +2,7 @@
     $nombre_form = $_POST['nombre'];
     $apellido_form = $_POST['apellido'];
     $email_form = $_POST['email'];
-    $novedades_form = $_POST['novedades'] ? 1 : 0;
+    $novedades_form = $_POST['novedades'] ;
 
     $data_mail = 
     "nombre:".$nombre_form . "\r\n" . 
@@ -11,15 +11,14 @@
     "novedades:".$novedades_form ;
 
     mail("juan.calvo.dev@gmail.com", "mensaje enviado desde nuestro sitio web",$data_mail);
-
-    $conexion = mysqli_connect('localhost','root','','php_utn') or exit("no se pudo conectar a la base de datos");
+    
+    $conexion = mysqli_connect('localhost','root','','juan_calvo_php') or exit("no se pudo conectar a la base de datos");
 
     mysqli_query($conexion,"INSERT INTO form_consulta VALUES(DEFAULT, '$nombre_form', '$apellido_form', '$email_form', '$novedades_form')");
 
     mysqli_close($conexion);
 
-    header("Location: tienda.php?ok");
+    header("Location:tienda.php?ok");
     
-    exit;
 
 ?>
--- tienda.php
+++ tienda.php
@@ -1,57 +1,119 @@
     <?php include("header.php")?>
+    <?php 
+        function cuotas($precio,$cuotas){
+            return $precio/$cuotas;
+        }    
+    ?>
     <main>
-       
-        <aside class="nav_sidebar">
-            <h2>Productos</h2>
-            <div class="botones_sidebar">
-                <a href="tienda.php?product=pcDesktop">Computadora de escritorio</a>
-                <a href="tienda.php?product=pcLaptop">Computadora portatil</a>
-                <a href="tienda.php?product=tablet">Tablet</a>
-            </ul>
-        </aside>
-        <?php 
-            $name = "Productos";
-            $specifications = "";
-            $price = null;
-            $img = "https://www.pcboxescomputacion.com.ar/img/brands.png";
+        <div class="side_and_product">
+            <aside class="nav_sidebar">
+                <h2>Productos</h2>
+                <div class="botones_sidebar">
+                    <a href="tienda.php?product=pcDesktop">Computadora de escritorio</a>
+                    <a href="tienda.php?product=pcLaptop">Computadora portatil</a>
+                    <a href="tienda.php?product=tablet">Tablet</a>
+                </ul>
+            </aside>
+            <?php 
+                $name = "Productos";
+                $specifications = "";
+                $price = null;
+                $img = "https://www.pcboxescomputacion.com.ar/img/brands.png";
+                $discount = 0;
 
-            if (isset($_GET['product'])){
-                switch ($_GET['product']){
-                    case 'pcDesktop':
-                        $name = "Computadora de escritorio";
-                        $specifications = "Procesador intel i7,video Geforce 3080,monitor ips 24 pulgadas curvo." ;
-                        $price = 2000;
-                        $img = "https://i.blogs.es/4dc651/huawei-mateview-gt-1-/1366_2000.jpeg";
-                        break;
-                    case 'pcLaptop':
-                        $name = "Computadora portatil";
-                        $specifications = "intel i5,SSD 500GB , Pantalla de 13 pulgas.";
-                        $price = 1200;
-                        $img = "https://consumer.huawei.com/content/dam/huawei-cbg-site/latam/mx/mkt/plp/laptops/d14-amd-2021.jpg";
-                        break;
-                    case 'tablet':
-                        $name = "Tablet";
-                        $specifications = "Tablet Samsung Pro, pantalla 10 pulgadas, 256gb , camara 15 megapixel.";
-                        $price = 500;
-                        $img = "https://cdn.mos.cms.futurecdn.net/bb65a58d396e629bd36bc1a5be972ce1.jpg";
-                        break;
+                $productos = array(
+                        array(
+                        'name' => "Placa de video",
+                        'specifications' => "Placa de video Nvidia, Rtx 4080",
+                        'price' => 1500,
+                        'img' => "https://mla-s1-p.mlstatic.com/620931-MLA52993222899_122022-F.jpg",
+                        'discount' => 25
+                    ),
+                        array(
+                        'name' => "Memoria Ram",
+                        'specifications' => "Memoria Ram Fury, 16gb Ddr4 3200mhz ",
+                        'price' => 150,
+                        'img' => "https://consumer.huawei.com/content/dam/huawei-cbg-site/latam/mx/mkt/plp/laptops/d14-amd-2021.jpg",
+                        'discount' => 5
+                    ),
+                        array(
+                        'name' => "Teclado mecanico",
+                        'specifications' => "Teclado mecanico Logistech Gaming Blanco",
+                        'price' => 200,
+                        'img' => "https://cdn.mos.cms.futurecdn.net/bb65a58d396e629bd36bc1a5be972ce1.jpg",
+                        'discount' => 10
+                    )
+                );
+
+                if (isset($_GET['product'])){
+                    switch ($_GET['product']){
+                        case 'pcDesktop':
+                            $name = "Computadora de escritorio";
+                            $specifications = "Procesador intel i7,video Geforce 3080,monitor ips 24 pulgadas curvo." ;
+                            $price = 2000;
+                            $img = "https://i.blogs.es/4dc651/huawei-mateview-gt-1-/1366_2000.jpeg";
+                            $discount = 10;
+                            break;
+                        case 'pcLaptop':
+                            $name = "Computadora portatil";
+                            $specifications = "intel i5,SSD 500GB , Pantalla de 13 pulgas.";
+                            $price = 1200;
+                            $img = "https://consumer.huawei.com/content/dam/huawei-cbg-site/latam/mx/mkt/plp/laptops/d14-amd-2021.jpg";
+                            $discount = 15;
+                            break;
+                        case 'tablet':
+                            $referencia =
+                            $name = "Tablet";
+                            $specifications = "Tablet Samsung Pro, pantalla 10 pulgadas, 256gb , camara 15 megapixel.";
+                            $price = 500;
+                            $img = "https://cdn.mos.cms.futurecdn.net/bb65a58d396e629bd36bc1a5be972ce1.jpg";
+                            $discount = 7;
+                            break;
+                    }
                 }
-            }
 
-        ?>
-        <section class="container_product">
-            <div class="product_description">
-                <h2><?php echo $name; ?></h2>
-                <div class="img_container">
-                    <img class="product_img" src="<?php echo $img; ?>" alt="">
-                </div>
-                
+            ?>
+            <section class="container_product">
+                <div class="product_description">
+                    <h2><?php echo $name; ?></h2>
+                    <div class="img_container">
+                        <img class="product_img" src="<?php echo $img; ?>" alt="">
+                    </div>
+                    
+                    <?php 
+                        function discount($valor,$porcentaje) {
+                            $porcentajeDecimal = $porcentaje / 100;
+                            $resultado = $valor - ($valor * $porcentajeDecimal);
+                        
+                            return $resultado;
+                        }
+                        if($price) {   
+                        echo '<p>Especificaciones:' . $specifications .'</p>';
+                        echo '<p class="price">Precio: u$S' . $price  .'</p>';
+                        echo '<p class="disconunt">descuento:'.$discount.'%</p>';
+                        echo '<p class="final_price">Precio final: u$s'.discount($price,$discount).'</p>';
+                        echo '<button class="comprar">Comprar</button>';
+                        }      
+                    ?>
+                </div>               
+            </section>
+        </div>
+        <section>
+            <h4 class="other_products">Otros productos...</h4>
+            <div class="container_cards_other_products">
                 <?php 
-                    if($price) {   
-                     echo '<p>Especificaciones:' . $specifications .'</p>';
-                     echo '<p class="price">Precio: u$S' . $price  .'</p>';
-                     echo '<button class="comprar">Comprar</button>';
-                    }      
+                foreach ($productos as $producto) {
+                    echo '<div class="card">';
+                    echo '<img src="' . $producto['img'] . '" alt="' . $producto['name'] . '">';
+                    echo '<div class="card-info">';
+                    echo '<h2>' . $producto['name'] . '</h2>';
+                    echo '<p>Precio: u$s' . $producto['price'] . '</p>';
+                    echo '<p>Descuento: ' . $producto['discount'] . '%</p>';
+                    echo '<p class="final_price_card">Precio final: u$s'.discount($producto['price'],$producto['discount']).'</p>';
+                    echo '<button class="comprar">Comprar</button>';
+                    echo '</div>';
+                    echo '</div>';
+                }
                 ?>
             </div>
         </section>
@@ -80,7 +142,7 @@
         </form>
         <?php 
             if (isset($_GET['ok'])){
-                echo "<h3>Su mensaje ha sido enviado con exito</h3>";
+                echo "<h3>Su mensaje ha sido enviado con exito!!</h3>";
             }
         ?>
         <p>
